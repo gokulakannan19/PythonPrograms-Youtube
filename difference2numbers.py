cf9ac0864fc454716459030c801b108bffbe82b9
gokulakannan19 <66069114+gokulakannan19@users.noreply.github.com>
2021-02-27 23:21:23 +0530

    difference between two numbers

---
+++ difference2numbers.py
@@ -0,0 +1,15 @@
+
+# getting input from the user
+number1 = input("Enter number1: ")
+number2 = input("Enter number1: ")
+
+# changing the input type to integer
+number1 = int(number1)
+number2 = int(number2)
+
+result = number1 - number2
+
+print(f'{number1} - {number2} = {result}')
+
+# Let's check
+# Try it out
