48f71b56836a9d495a36d3fdf4f88e235b96e247
gokulakannan19 <66069114+gokulakannan19@users.noreply.github.com>
2021-03-01 22:23:14 +0530

    Biggest and Smallest of two numbers

---
+++ BiggestOf2Numbers.py
@@ -0,0 +1,8 @@
+number1 = int(input("Enter number1: "))
+number2 = int(input("Enter number2: "))
+
+if number1 > number2:
+    print(f'{number1} is bigger')
+else:
+    print(f'{number2} is bigger')
+
---
+++ SmallestOf2Numbers.py
@@ -0,0 +1,11 @@
+number1 = int(input("Enter number1: "))
+number2 = int(input("Enter number2: "))
+
+if number1 < number2:
+    print(f'{number1} is smaller')
+else:
+    print(f'{number2} is smaller')
+
+#Lets check
+# Hope you enjoyed
+# Try it out
