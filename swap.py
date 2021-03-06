ecefb355c12da37c8a548c6c3ee664f52da17754
gokulakannan19 <66069114+gokulakannan19@users.noreply.github.com>
2021-03-06 12:39:11 +0530

    swapping programs added

---
+++ swap.py
@@ -0,0 +1,16 @@
+
+print("python program to swap two numbers")
+
+a = 5
+b = 10
+
+print("Before swapping")
+print(f'a = {a}, b = {b}')
+
+#using temp variable
+temp = a
+a = b
+b = temp
+
+print("After swapping")
+print(f'a = {a}, b = {b}')
---
+++ swap2.py
@@ -0,0 +1,16 @@
+
+print("python program to swap two numbers")
+
+a = 5
+b = 10
+
+print("Before swapping")
+print(f'a = {a}, b = {b}')
+
+# using + and -
+a = a+b
+b = a-b
+a = a-b
+
+print("After swapping")
+print(f'a = {a}, b = {b}')
